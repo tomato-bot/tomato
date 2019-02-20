ddb92e54a98ef17d080a39f0de0e1459c02f98d7
Moshe Zada <zada.moshe@gmail.com>
2019-02-20 13:16:44 +0200

    make plugin create xml if to passed

--- pytest_tomato/plugin.py
+++ pytest_tomato/plugin.py
@@ -1,6 +1,7 @@
 import json
 import logging
-from os import environ
+import tempfile
+from os import environ, remove
 
 try:
     from urllib2 import urlopen, Request
@@ -69,6 +70,23 @@ class CircleCi(CI):
         )
 
 
+state = {
+    "created": False,
+    "path": None,
+}
+
+
+def pytest_load_initial_conftests(args):
+    if '--junit-xml' in args or '--junitxml' in args:
+        return
+    fp = tempfile.NamedTemporaryFile(delete=False)
+    logger.info('Creating tomato.xml file due to missing --junit-xml flag')
+    state["created"] = True
+    state["path"] = fp.name
+    fp.close()
+    args[:] = ["--junit-xml", state["path"]] + args
+
+
 def pytest_sessionfinish(session):
     if session.config.option.xmlpath is None:
         logger.warning("Tomato plugin disabled due to missing --junit-xml flag")
@@ -80,4 +98,6 @@ def pytest_sessionfinish(session):
         data.update(ci.parse())
         logger.debug("Detected CI environment - %s", data['client'])
         post(data)
+        if state["created"] is True:
+            remove(state["path"])
         return
