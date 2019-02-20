cff050211e294aa7adfd8c638c2a417f8159a07f
Moshe Zada <zada.moshe@gmail.com>
2019-02-20 18:46:19 +0200

    Support appveyor

--- pytest-tomato/setup.py
+++ pytest-tomato/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-install_requires = ["tomato-lib==1.0.3"]
+install_requires = ["tomato-lib==1.0.4"]
 
 
 try:
@@ -11,7 +11,7 @@ except Exception:
 
 setup(
     name="pytest-tomato",
-    version='1.0.3',
+    version='1.0.4',
     description="",
     long_description=long_description,
     author="Moshe Zada",
