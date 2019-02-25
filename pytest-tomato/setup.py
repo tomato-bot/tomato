d7765c245c7e9b9ea12b179007561123cf437b27
Moshe Zada <zada.moshe@gmail.com>
2019-02-25 19:04:49 +0200

    New version

--- pytest-tomato/setup.py
+++ pytest-tomato/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-version = '1.0.13'
+version = '1.0.14'
 install_requires = [f"tomato-lib=={version}"]
 
 
--- tomato-lib/setup.py
+++ tomato-lib/setup.py
@@ -11,7 +11,7 @@ except Exception:
 
 setup(
     name="tomato_lib",
-    version='1.0.13',
+    version='1.0.14',
     description="",
     long_description=long_description,
     author="Moshe Zada",
